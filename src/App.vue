020390298cc154b2017e117e3cfdee0620199a45
s-moe <sarahannmoe@gmail.com>
2021-10-28 09:31:14 -0700

    app created

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "012344",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Lorenzo",
+          phone: "012344",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
