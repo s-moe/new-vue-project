a577345afbfb12ba1c6c41ecbecae07f74e09af9
s-moe <sarahannmoe@gmail.com>
2021-10-29 07:17:44 -0700

    binding props to dynamic values

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite == "1" ? "Favorite" : "" }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite ? "Favorite" : "" }}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
