9ce19fd1788ed4c5c4bdd907b864b6745c38c292
Sarah Moe <sarahannmoe@gmail.com>
2021-11-03 16:36:38 -0700

    added delete to emits array

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -48,7 +48,7 @@ export default {
       // },
     },
   },
-  emits: ["toggle-favorite"],
+  emits: ["toggle-favorite", "delete"],
   // emits: {
   //   "toggle-favorite": function (id) {
   //     if (id) {
